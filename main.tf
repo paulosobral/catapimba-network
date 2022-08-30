21ae5946b51ed2d43a3da874240ea4aa9e02bf83
Paulo Sobral <paulo.sobral@outlook.com.br>
2022-08-29 21:32:31 -0300

    feat: create variables

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "catapimba-corps-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs             = var.subnet_azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = false
-  one_nat_gateway_per_az = true
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
 
   tags = {
     Terraform   = "true"
